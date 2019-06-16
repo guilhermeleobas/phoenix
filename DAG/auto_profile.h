1b6d09904645b187ee5954e0f965029426af3686
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-06-16 15:56:02 -0300

    fix error in auto profilling technique

--- DAG/auto_profile.h
+++ DAG/auto_profile.h
@@ -22,10 +22,10 @@
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 
-#include "utils.h"
 #include "insertIf.h"
+#include "utils.h"
 
-namespace phoenix{
+namespace phoenix {
 
 #define SWITCH_NUM_CASES 3
 #define SWITCH_BB_PROFILE_INDEX 0
@@ -44,35 +44,30 @@ namespace phoenix{
 //   2 => jumps to BBOpt
 //
 // returns the instruction that controls the switch jump target
-Instruction *create_switch(Function *F, BasicBlock *BB,
-                                BasicBlock *BBProfile, BasicBlock *BBOpt) {
+Instruction *create_switch(Function *F, BasicBlock *BB, BasicBlock *BBProfile, BasicBlock *BBOpt) {
   // 1. Create a variable to control the switch
   IRBuilder<> Builder(F->getEntryBlock().getFirstNonPHI());
-  Instruction *switch_control_ptr = Builder.CreateAlloca(
-      Type::getInt32Ty(F->getContext()), nullptr, "switch.control_ptr");
-  Builder.CreateStore(ConstantInt::get(Type::getInt32Ty(F->getContext()),
-                                       SWITCH_BB_PROFILE_INDEX),
+  Instruction *switch_control_ptr =
+      Builder.CreateAlloca(Type::getInt32Ty(F->getContext()), nullptr, "switch.control_ptr");
+  Builder.CreateStore(ConstantInt::get(Type::getInt32Ty(F->getContext()), SWITCH_BB_PROFILE_INDEX),
                       switch_control_ptr);
 
   // 2. Create the switch with a default jump to BBProfile
-  BasicBlock *BBSwitch =
-      BasicBlock::Create(F->getContext(), "Switch", F, BB);
+  BasicBlock *BBSwitch = BasicBlock::Create(F->getContext(), "Switch", F, BB);
   Builder.SetInsertPoint(BBSwitch);
   Instruction *load = Builder.CreateLoad(switch_control_ptr, "switch_control");
   SwitchInst *si = Builder.CreateSwitch(load, BBProfile, SWITCH_NUM_CASES);
 
   // Case with 1 with a jump to BB
-  si->addCase(
-      ConstantInt::get(Type::getInt32Ty(F->getContext()), SWITCH_BB_INDEX), BB);
+  si->addCase(ConstantInt::get(Type::getInt32Ty(F->getContext()), SWITCH_BB_INDEX), BB);
 
   // Case 2 with a jump to the optimized BB
-  si->addCase(
-      ConstantInt::get(Type::getInt32Ty(F->getContext()), SWITCH_BB_OPT_INDEX),
-      BBOpt);
+  si->addCase(ConstantInt::get(Type::getInt32Ty(F->getContext()), SWITCH_BB_OPT_INDEX), BBOpt);
 
   // Iterate over every predecessor of BB and changes it's jump to switchBB
   for (BasicBlock *pred : predecessors(BB)) {
-    if (pred == BBSwitch) continue;
+    if (pred == BBSwitch)
+      continue;
     TerminatorInst *TI = pred->getTerminator();
 
     switch (TI->getOpcode()) {
@@ -102,8 +97,7 @@ AllocaInst *create_c1(Function *F, BasicBlock *BBProfile) {
   auto *zero = ConstantInt::get(I32Ty, 0);
   auto *one = ConstantInt::get(I32Ty, 1);
 
-  AllocaInst *c1_ptr =
-      Builder.CreateAlloca(I32Ty, nullptr, "c1_ptr." + BBProfile->getName());
+  AllocaInst *c1_ptr = Builder.CreateAlloca(I32Ty, nullptr, "c1_ptr." + BBProfile->getName());
   Builder.CreateStore(zero, c1_ptr);
 
   // c1 inc
@@ -117,26 +111,24 @@ AllocaInst *create_c1(Function *F, BasicBlock *BBProfile) {
 }
 
 // Create and increment c2 when V == constant
-AllocaInst *create_c2(Function *F, BasicBlock *BBProfile, Value *V,
-                           Value *constant) {
+AllocaInst *create_c2(Function *F, BasicBlock *BBProfile, Value *before, Value *after) {
   IRBuilder<> Builder(F->getEntryBlock().getFirstNonPHI());
 
   auto *I32Ty = Type::getInt32Ty(F->getContext());
   auto *zero = ConstantInt::get(I32Ty, 0);
   auto *one = ConstantInt::get(I32Ty, 1);
 
-  AllocaInst *c2_ptr =
-      Builder.CreateAlloca(I32Ty, nullptr, "c2_ptr." + BBProfile->getName());
+  AllocaInst *c2_ptr = Builder.CreateAlloca(I32Ty, nullptr, "c2_ptr." + BBProfile->getName());
   Builder.CreateStore(zero, c2_ptr);
 
   // c2 inc
-  Builder.SetInsertPoint(cast<Instruction>(V)->getNextNode());
+  Builder.SetInsertPoint(cast<Instruction>(after)->getNextNode());
 
   Value *cmp;
-  if (V->getType()->isFloatingPointTy())
-    cmp = Builder.CreateFCmpOEQ(V, constant, "cmp.profile");
+  if (before->getType()->isFloatingPointTy())
+    cmp = Builder.CreateFCmpOEQ(before, after, "cmp.profile");
   else
-    cmp = Builder.CreateICmpEQ(V, constant, "cmp.profile");
+    cmp = Builder.CreateICmpEQ(before, after, "cmp.profile");
 
   Value *select = Builder.CreateSelect(cmp, zero, one);
 
@@ -156,10 +148,12 @@ AllocaInst *create_c2(Function *F, BasicBlock *BBProfile, Value *V,
 // and insert counters c1 and c2 to:
 //   - @c1: Count the number of times @BBProfile executes.
 //   - @c2: Count the number of times @V == @onstraint
-std::pair<Value*, Value*> create_BBProfile(Function *F, ValueToValueMapTy &VMap, BasicBlock *BBProfile, Value *V,
-                      Value *constant) {
+std::pair<Value *, Value *> create_BBProfile(Function *F,
+                                             BasicBlock *BBProfile,
+                                             Instruction *before,
+                                             Instruction *after) {
   Value *c1 = create_c1(F, BBProfile);
-  Value *c2 = create_c2(F, BBProfile, cast<Value>(VMap[V]), constant);
+  Value *c2 = create_c2(F, BBProfile, before, after);
 
   return std::make_pair(c1, c2);
 }
@@ -171,8 +165,7 @@ std::pair<Value*, Value*> create_BBProfile(Function *F, ValueToValueMapTy &VMap,
 // @constant: the value that @V must hold to kill the expression
 //
 // Returns a clone of @BB with the conditional. See `insert_if` for more info.
-void create_BBOpt(ValueToValueMapTy &VMap, StoreInst *store, Value *V,
-                       Value *constant) {
+void create_BBOpt(ValueToValueMapTy &VMap, StoreInst *store, Value *V, Value *constant) {
   insert_if(cast<StoreInst>(VMap[store]), VMap[V], constant);
 }
 
@@ -196,10 +189,13 @@ void create_BBOpt(ValueToValueMapTy &VMap, StoreInst *store, Value *V,
 // Creates a basic block that changes the value of @switch_control based
 // on the counters.
 //
-void create_BBControl(Function *F, BasicBlock *BBProfile,
-                           Value *switch_control_ptr, Value *c1_ptr,
-                           Value *c2_ptr, ConstantInt *n_iter,
-                           ConstantInt *gap) {
+void create_BBControl(Function *F,
+                      BasicBlock *BBProfile,
+                      Value *switch_control_ptr,
+                      Value *c1_ptr,
+                      Value *c2_ptr,
+                      ConstantInt *n_iter,
+                      ConstantInt *gap) {
   auto *I32Ty = Type::getInt32Ty(F->getContext());
   auto *BBOpt_target_value = ConstantInt::get(I32Ty, SWITCH_BB_OPT_INDEX);
   auto *BB_target_value = ConstantInt::get(I32Ty, SWITCH_BB_INDEX);
@@ -208,34 +204,33 @@ void create_BBControl(Function *F, BasicBlock *BBProfile,
   auto *zero = ConstantInt::get(I32Ty, 0);
 
   // Create BBControl and insert it right after BBProfile
-  BasicBlock *BBControl =
-      BBProfile->splitBasicBlock(BBProfile->getTerminator(), "BBControl");
+  BasicBlock *BBControl = BBProfile->splitBasicBlock(BBProfile->getTerminator(), "BBControl");
   IRBuilder<> Builder(BBControl->getFirstNonPHI());
 
   Value *c1 = Builder.CreateLoad(c1_ptr, "c1");
   Value *c2 = Builder.CreateLoad(c2_ptr, "c2");
   Value *sub = Builder.CreateSub(c1, c2, "c1-c2");
-  Value *switch_control =
-      Builder.CreateLoad(switch_control_ptr, "switch_control");
+  Value *switch_control = Builder.CreateLoad(switch_control_ptr, "switch_control");
 
   // if c1 - c2 > gap then we change switch_control to jump to BB, otherwise,
   // jump to BBOpt
   Value *gap_cmp = Builder.CreateICmpSGE(sub, gap, "gap.cmp");
-  Value *new_target =
-      Builder.CreateSelect(gap_cmp, BB_target_value, BBOpt_target_value);
+  Value *new_target = Builder.CreateSelect(gap_cmp, BB_target_value, BBOpt_target_value);
 
   // decide if it is time to change the switch jump
   Value *iter_cmp = Builder.CreateICmpEQ(c1, n_iter, "iter.cmp");
-  Value *n_switch_control = Builder.CreateSelect(
-      iter_cmp, new_target, switch_control, "new_switch_control");
+  Value *n_switch_control =
+      Builder.CreateSelect(iter_cmp, new_target, switch_control, "new_switch_control");
 
   // Save the value
   Builder.CreateStore(n_switch_control, switch_control_ptr);
 }
 
-void create_BBControl(Function *F, BasicBlock *BBProfile,
-                           Value *switch_control_ptr, Value *c1_ptr,
-                           Value *c2_ptr) {
+void create_BBControl(Function *F,
+                      BasicBlock *BBProfile,
+                      Value *switch_control_ptr,
+                      Value *c1_ptr,
+                      Value *c2_ptr) {
 #define N_ITER 1000
 #define GAP 501
 
@@ -243,14 +238,11 @@ void create_BBControl(Function *F, BasicBlock *BBProfile,
   auto *n_iter = ConstantInt::get(I32Ty, N_ITER);
   auto *gap = ConstantInt::get(I32Ty, GAP);
 
-  create_BBControl(F, BBProfile, switch_control_ptr, c1_ptr, c2_ptr, n_iter,
-                   gap);
+  create_BBControl(F, BBProfile, switch_control_ptr, c1_ptr, c2_ptr, n_iter, gap);
 }
 
-
 // finds all users of @I that are outside @BB
-std::vector<Instruction *> find_usages_outside_BB(BasicBlock *BB,
-                                                       Instruction *I) {
+std::vector<Instruction *> find_usages_outside_BB(BasicBlock *BB, Instruction *I) {
   std::vector<Instruction *> v;
 
   for (User *user : I->users()) {
@@ -266,9 +258,12 @@ std::vector<Instruction *> find_usages_outside_BB(BasicBlock *BB,
 // basic blocks. and because we insert @BBProfile and @BBOpt, @I stop
 // dominates all its uses. In those cases we need to create PHI nodes to fix
 // the uses!
-void create_phi_nodes(Function *F, BasicBlock *BB, BasicBlock *BBProfile,
-                           BasicBlock *BBOpt, ValueToValueMapTy &VMapProfile,
-                           ValueToValueMapTy &VMapOpt) {
+void create_phi_nodes(Function *F,
+                      BasicBlock *BB,
+                      BasicBlock *BBProfile,
+                      BasicBlock *BBOpt,
+                      ValueToValueMapTy &VMapProfile,
+                      ValueToValueMapTy &VMapOpt) {
   BasicBlock *BBPhi = BB->splitBasicBlock(BB->getTerminator(), "BBPhi");
   IRBuilder<> Builder(BBPhi->getFirstNonPHI());
 
@@ -298,13 +293,12 @@ void create_phi_nodes(Function *F, BasicBlock *BB, BasicBlock *BBProfile,
         Value *op = Inst->getOperand(i);
         if (!isa<Instruction>(op))
           continue;
-        if (cast<Instruction>(op) == &I){
+        if (cast<Instruction>(op) == &I) {
           DEBUG(errs() << "replacing " << *op << " -> " << *phi << "\n");
           Inst->setOperand(i, phi);
         }
       }
     }
-
   }
 
   // Change the branch inst to jump straight to BBPhi
@@ -312,11 +306,16 @@ void create_phi_nodes(Function *F, BasicBlock *BB, BasicBlock *BBProfile,
   BBOpt->getTerminator()->setSuccessor(0, BBPhi);
 }
 
-
 //
-void auto_profile(Function *F, StoreInst *store, NodeSet &s){
-  phoenix::Node *node = *s.begin();
-  BasicBlock *BB = node->getInst()->getParent();
+void auto_profile(Function *F, ReachableNodes &rn) {
+  // load = LoadInst *ptr
+  // arith = op @load @other_inst
+  // store @arith, *ptr
+  StoreInst *store = rn.get_store();
+  LoadInst *load = rn.get_load();
+  Instruction *arith = rn.get_arith_inst();
+
+  BasicBlock *BB = store->getParent();
 
   ValueToValueMapTy VMapOpt;
   BasicBlock *BBOpt = deep_clone(BB, VMapOpt, ".opt", F);
@@ -324,17 +323,16 @@ void auto_profile(Function *F, StoreInst *store, NodeSet &s){
   ValueToValueMapTy VMapProfile;
   BasicBlock *BBProfile = deep_clone(BB, VMapProfile, ".profile", F);
 
-  assert(node->hasConstant() && "Node do not have a constant");
-
   create_phi_nodes(F, BB, BBProfile, BBOpt, VMapProfile, VMapOpt);
 
-  create_BBOpt(VMapOpt, store, node->getValue(),
-               node->getConstant());
+  for (phoenix::Node *node : rn.get_nodeset())
+    create_BBOpt(VMapOpt, store, node->getValue(), node->getConstant());
 
-  Value *V = node->getValue();
-  Value *constant = node->getConstant();
+  // Let's profile the store to see if it is silent
+  // if load == arith then the store will be silent
+  auto p = create_BBProfile(F, BBProfile, cast<Instruction>(VMapProfile[load]),
+                            cast<Instruction>(VMapProfile[arith]));
 
-  auto p = create_BBProfile(F, VMapProfile, BBProfile, V, constant);
   Value *c1 = p.first;
   Value *c2 = p.second;
 
@@ -343,11 +341,15 @@ void auto_profile(Function *F, StoreInst *store, NodeSet &s){
   create_BBControl(F, BBProfile, switch_control_ptr, c1, c2);
 }
 
-void auto_profile(Function *F, std::vector<ReachableNodes> &reachables){
-  for (ReachableNodes &r : reachables){
-    NodeSet nodes = r.get_nodeset();
-    auto_profile(F, r.get_store(), nodes);
+void auto_profile(Function *F, std::vector<ReachableNodes> &reachables) {
+  if (reachables.empty())
+    return;
+
+  for (ReachableNodes &rn : reachables) {
+    NodeSet nodes = rn.get_nodeset();
+    if (nodes.size())
+      auto_profile(F, rn);
   }
 }
 
-}; // end namespace phoenix
+};  // end namespace phoenix
